44f01a43048b406844ceade2706f0b7d840c7bc9
hhhirokunnn <m2017hiro@gmail.com>
2024-08-04 15:55:27 +0900

    Create split-bst.py

---
+++ recursion/split-bst.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/split-bst/submissions/
+class Solution:
+    def splitBST(self, root: Optional[TreeNode], target: int) -> List[Optional[TreeNode]]:
+        if not root:
+            return [None, None]
+        
+        if root.val > target:
+            result = self.splitBST(root.left, target)
+            root.left = result[1]
+            return [result[0], root]
+        else:
+            result = self.splitBST(root.right, target)
+            root.right = result[0]
+            return [root, result[1]]
+
+class Solution:
+    def splitBST(self, root: Optional[TreeNode], target: int) -> List[Optional[TreeNode]]:
+        ans = [None, None]
+        if not root:
+            return ans
+        cur = root
+        stack = []
+        
+        while cur:
+            stack.append(cur)
+            if cur.val > target:
+                cur = cur.left
+            else:
+                cur = cur.right
+        while stack:
+            node = stack.pop()
+            if node.val > target:
+                node.left = ans[1]
+                ans[1] = node
+            else:
+                node.right = ans[0]
+                ans[0] = node
+        return ans
